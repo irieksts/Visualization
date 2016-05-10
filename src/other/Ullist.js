7b2c35c559568b7b22b1f7478b81f860bffcc058
irieksts <isaac.rieksts@risk.lexisnexis.com>
2016-05-10 15:09:23 -0400

    got some recursive working

--- src/other/Ullist.js
+++ src/other/Ullist.js
@@ -17,40 +17,71 @@
 
     Ullist.prototype.enter = function (domNode, element) {
         HTMLWidget.prototype.enter.apply(this, arguments);
-        this._ul = element.append("ul");
+        this._ul = element.append("table");
     };
 
     		Ullist.prototype.update = function (domNode, element) {
         HTMLWidget.prototype.update.apply(this, arguments);
-        var depth = 0;
-        var li = this._ul.selectAll(".innerRow" + depth).data(this.data());
-        li.enter().append("li")
-            .attr("class", "innerRow" + depth)
-        ;
-        li.text(function(d) {return d[0];
-        	}).each (function (d) {
-            depth = 1;
-          	var liE = d3.select(this);
-          	var innerUl = liE.append("ul");
-          	var inner = innerUl.selectAll(".innerRow" + depth).data(d[1]);
-          	inner.enter().append("li")
-          	.attr("class", "innerRow" + depth);
+        this.appendArray(this._ul, this.data());
+        
+        // var depth = 0;
+        // var li = this._ul.selectAll("ul#parent > .row.innerRow").data(this.data());
+        // li.enter().append("li")
+            // .attr("class", "row innerRow")
+        // ;
+        // li.text(function(d) {return d[0];
+        	// }).each (function (d) {
+            // depth = 1;
+          	// var liE = d3.select(this);
+          	// var innerUl = liE.append("ul");
+          	// var inner = innerUl.selectAll("ul#parent > .row.innerRow").data(d[1]);
+          	// inner.enter().append("li")
+          	// .attr("class", "row innerRow");
+          	// inner
+          	  // .text(function (d2) {
+          	  	// return d2[0];
+          	  // });
+          	 // inner.exit().remove();
+//           	
+          // });
+        // ;
+//         
+        // li.exit().remove();
+        
+    };
+    
+    Ullist.prototype.appendArray = function(element, arr) {
+    	//var context = element;
+    //	var innerUl = liE.append("ul");
+   // 	li.text(function(d) {return d[0];
+    //    	}).each (function (d) {
+          	//var liE = element;
+          	var innerUl = element.append("tr");
+          	var inner = innerUl.selectAll("ul#parent > .dataRow").data(arr);
+          	inner.enter().append("td")
+          	.attr("class", "dataRow");
+          	var context = this;
           	inner
           	  .text(function (d2) {
           	  	return d2[0];
-          	  });
+          	  }).
+          	  each(function(d) {
+          	  	if(d[1] instanceof Array){
+          	  		context.appendArray(d3.select(this), d[1]);
+          	  	}
+          	  }
+          	  );
           	 inner.exit().remove();
           	
-          });
+         // });
         ;
-        
-        li.exit().remove();
-        
     };
 
     Ullist.prototype.exit = function (domNode, element) {
         HTMLWidget.prototype.exit.apply(this, arguments);
     };
+    
+    
 
     return Ullist;
 }));
--- test/otherFactory.js
+++ test/otherFactory.js
@@ -245,17 +245,10 @@
                 	callback(new UlList()
                         .columns(["Col A", "Col B"])
                         .data([
-							["GJS0", [["CHILD101", 7], ["CHILDn02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
+							["GJS0", [["CHILD101", [["ABC", 7], ["ABC4", 7], ["CHILD003", [["AB", "a"]]], ["CHILD004", 7]]], ["CHILDn02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
 							["GJS0", [["CHILD201", 7], ["CHILDm02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
 							["GJS0", [["CHILD301", 7], ["CHILDl02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD401", 7], ["CHILDk02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD501", 7], ["CHILDj02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD601", 7], ["CHILDi02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD701", 7], ["CHILDh02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD801", 7], ["CHILDg02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILD901", 7], ["CHILDf02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILDa01", 7], ["CHILDe02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7],
-							["GJS0", [["CHILDc01", 7], ["CHILDd02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7]
+							["GJS0", [["CHILD701", 7], ["CHILDh02", 7], ["CHILD003", 7], ["CHILD004", 7]], 7]
                         ])
                     );
                 });
